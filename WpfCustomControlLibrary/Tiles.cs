cb407b5af08b8f0c0cf4a1056ae7a6ccd04c1cd6
Raphael <Raphael@RAPHAEL-LAPTOP>
2018-11-26 10:30:39 +0800

    Add Tiles custom control

---
+++ WpfCustomControlLibrary/Tiles.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfCustomControlLibrary
+{
+    public class Tiles : Button
+    {
+        static Tiles()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Tiles), new FrameworkPropertyMetadata(typeof(Tiles)));
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register(
+            "ImageSource", typeof(ImageSource), typeof(Tiles), new PropertyMetadata(default(ImageSource)));
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource) GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+    }
+}
